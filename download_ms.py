c434f486818cc494d97aaed326208d0c0444e30b
franklintimoteo <franklintimoteo@live.com>
2021-07-20 23:50:08 -0300

    Implementa uma alternativa caso a página não seja encontrada. Algo como NotFound é encontrado na exception do webdriver. Adicionado também a possibilidade do sistema ser linux, caso seja, o diretório de descompactação é configurado para /tmp/, e sua respectiva configuração na extração na linha 74, também é modificada. fix #1

--- download_ms.py
+++ download_ms.py
@@ -10,6 +10,7 @@
 
 from glob import glob
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.firefox.options import Options
 import time
 import os
@@ -19,13 +20,15 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 # In[2]:
-
+OS_SYSTEM = os.name # nt == windows posix == linux, unix
+# caso seja um sistema diferente de windows, obtem um diretório apropriado
+BASE_FOLDER = "C:\\covid-amazonas\\" if 'nt' in OS_SYSTEM else '/tmp/'
 ## Define a pasta onde será baixado o arquivo e deleta arquivos (`RAR` e `CSV`s extraídos) antes
-folder = "C:\\covid-amazonas\\"
 
-for item in os.listdir(folder):
+
+for item in os.listdir(BASE_FOLDER):
     if item.endswith(tuple([".rar", ".csv"])):
-        os.remove(os.path.join(folder, item))
+        os.remove(os.path.join(BASE_FOLDER, item))
 
 # In[3]:
 
@@ -34,18 +37,26 @@ for item in os.listdir(folder):
 options = Options()
 options.set_preference("browser.download.folderList", 2) # diretorio de download personalizado
 options.set_preference("browser.download.manager.showWhenStarting", False) # nao abrir caixa de dialogo
-options.set_preference("browser.download.dir", folder) # baixar na pasta personalizada
+options.set_preference("browser.download.dir", BASE_FOLDER) # baixar na pasta personalizada
 options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-rar-compressed, application/octet-stream, application/zip, application/octet-stream, application/x-zip-compressed, multipart/x-zip") # nao perguntar sobre arquivos compactados
 browser = webdriver.Firefox(options=options) # abre firefox
 
+while True:
+    try:   
+        # entra no site
+        browser.get("https://covid.saude.gov.br/")
+        # espera 15 segundos para carregar todo site
+        time.sleep(15)
+    except WebDriverException as error:
+        # Caso esteja sem internet, ou website indisponível ocorrera um erro com NotFound em error.msg
+        # Ingnora silenciosamente e tenta uma nova requisição
+        time.sleep(25) # faz uma nova tentativa após 25 segundos
+        pass
+    else:
+        # Se tudo ocorreu bem no browser.get, sai do while
+        break
 
-# entra no site
-browser.get("https://covid.saude.gov.br/")
-# espera 15 segundos para carregar todo site
-time.sleep(15)  
-
-
-filename = glob('C:\\covid-amazonas\\*.rar')
+filename = glob(BASE_FOLDER+'*.rar')
 
 # clica em baixar arquivo
 browser.find_element_by_xpath("//ion-button[contains(text(),'Arquivo')]").click()
@@ -53,14 +64,14 @@ browser.find_element_by_xpath("//ion-button[contains(text(),'Arquivo')]").click(
 # assim que baixar o arquivo .rar, fecha o browser
 while not filename:
     time.sleep(1)
-    filename = glob('C:\\covid-amazonas\\*.rar')
+    filename = glob(BASE_FOLDER+'*.rar')
 
 browser.quit()
-    
 # In[7]:
 
 # extraindo o arquivo RAR
-patoolib.extract_archive(filename, outdir=folder)
+program = '/usr/bin/7z' if 'posix' in OS_SYSTEM else ''
+patoolib.extract_archive(filename, outdir=BASE_FOLDER, program=program)
                          
 
 # In[8]:
@@ -68,9 +79,9 @@ patoolib.extract_archive(filename, outdir=folder)
 # juntando os 3 CSVs que vem dentro do rar em um único dataframe, filtrando em seguida apenas dados do AM
 csvs = []
 
-for table in os.listdir(folder):
+for table in os.listdir(BASE_FOLDER):
     if table.endswith(".csv"):
-        df = pd.read_csv(folder + table, index_col=None, header=0, sep=';')
+        df = pd.read_csv(BASE_FOLDER + table, index_col=None, header=0, sep=';')
         csvs.append(df)
 
 df_all = pd.concat(csvs, axis=0, ignore_index=True)
@@ -109,5 +120,5 @@ df_am['casos_100k'] = (df_am['casosAcumulado'] / df_am['populacaoTCU2019']) * 10
 # In[54]:
 
 # salva csv final
-df_am.to_csv(folder + 'casos_am_MS.csv', sep=',', encoding='utf-8', index=False)
+df_am.to_csv(BASE_FOLDER + 'casos_am_MS.csv', sep=',', encoding='utf-8', index=False)
 # df_all.to_csv(folder + 'casos_brasil_MS.csv', sep=',', encoding='utf-8', index=False)
